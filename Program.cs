40705070e24d41c7d3d7cc498b7582511b7bdfaa
AntonSmovzhenko <84539906+AntonSmovzhenko@users.noreply.github.com>
2022-02-19 22:13:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Csharp_Lab1_SmovzhenkoAnton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
